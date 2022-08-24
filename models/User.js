ba6d0c0d968b9fd03eed50881863216ebac64091
abdelrahman <gadoua50@gmail.com>
2022-08-24 20:22:54 +0200

    delete salt propert from user document

--- models/User.js
+++ models/User.js
@@ -6,7 +6,6 @@ const userSchema = new Schema({
   firstname: { type: String, required: true, maxLength: 100 },
   lastname: { type: String, required: true, maxLength: 100 },
   email: { type: String, required: true, maxLength: 100 },
-  salt: { type: String, required: true },
   password: { type: String, required: true },
 });
 
