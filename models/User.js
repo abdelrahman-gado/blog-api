9e56c7d48c6c953507ea65a163a528cf13cce83c
abdelrahman <gadoua50@gmail.com>
2022-08-24 16:31:06 +0200

    create user model

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstname: { type: String, required: true, maxLength: 100 },
+  lastname: { type: String, required: true, maxLength: 100 },
+  email: { type: String, required: true, maxLength: 100 },
+  salt: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+userSchema.virtual("fullname").get(function () {
+  return this.firstname + " " + this.lastname;
+});
+
+module.exports = mongoose.model("User", userSchema);
