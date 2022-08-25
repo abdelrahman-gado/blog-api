5bfe1dc0e00f86a39288fda5cf428a1b303a36e2
abdelrahman <gadoua50@gmail.com>
2022-08-25 20:53:55 +0200

    implement jwt strategy

--- routes/create.js
+++ routes/create.js
@@ -4,6 +4,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
 
 const router = express.Router();
 
@@ -37,6 +39,26 @@ passport.use(
       });
     }
   )
+);
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET;
+
+passport.use(
+  new jwtStrategy(opts, (jwt_payload, done) => {
+    User.findById(jwt_payload.userId, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+
+      if (!user) {
+        return done(null, false);
+      } else {
+        return done(null, jwt_payload.userId);
+      }
+    });
+  })
 );
 
 router.post("/signup", createController.signup);
