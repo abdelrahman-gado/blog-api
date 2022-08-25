d8dc4b55e0399b2da8d7222b121ebd3bf33fee62
abdelrahman <gadoua50@gmail.com>
2022-08-25 15:41:28 +0200

    implement passport local strategy

--- routes/create.js
+++ routes/create.js
@@ -1,7 +1,43 @@
 const express = require("express");
 const createController = require("../controllers/createController");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (email, password, done) => {
+      User.findOne({ email: email }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+
+        if (!user) {
+          return done(null, false, {
+            message: "Incorrect Username or Password!!",
+          });
+        }
+
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (result) {
+            return done(null, user);
+          } else {
+            return done(null, false, {
+              message: "Incorrect Username or Password!",
+            });
+          }
+        });
+      });
+    }
+  )
+);
 
 router.post("/signup", createController.signup);
 
