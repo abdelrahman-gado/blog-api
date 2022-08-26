8af7a28c3e825951bc11bad4dc963420429d24f8
abdelrahman <gadoua50@gmail.com>
2022-08-26 16:08:48 +0200

    implement get all posts controller

--- controllers/displayController.js
+++ controllers/displayController.js
@@ -1,5 +1,20 @@
+const Post = require("../models/Post");
+
 exports.getAllPosts = (req, res, next) => {
-  res.json({ message: "Not implemneted YET" });
+  Post.find({})
+    .populate("author", "-_id firstname lastname email")
+    .select("-_id title content timestamp published")
+    .exec((err, posts) => {
+      if (err) {
+        return res.status(400).json({ message: "Error in getting posts" });
+      }
+
+      if (posts.length === 0) {
+        return res.status(204).json({ message: "No Content" });
+      }
+
+      return res.status(200).json({ posts });
+    });
 };
 
 exports.getSpecificPost = (req, res, next) => {
