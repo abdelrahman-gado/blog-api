32781dd2751144b6d9e1ab4604ff90489b65b2ac
abdelrahman <gadoua50@gmail.com>
2022-08-26 11:48:41 +0200

    add initial controllers without implement

---
+++ controllers/displayController.js
@@ -0,0 +1,19 @@
+exports.getAllPosts = (req, res, next) => {
+  res.json({ message: "Not implemneted YET" });
+};
+
+exports.getSpecificPost = (req, res, next) => {
+  res.json({ message: "Not implemneted YET" });
+};
+
+exports.getCommentsOfSpecificPost = (req, res, next) => {
+  res.json({ message: "Not implemneted YET" });
+};
+
+exports.createCommentOnSpecificPost = (req, res, next) => {
+  res.json({ message: "Not implemneted YET" });
+};
+
+exports.getSpecificCommentOfSpecificPost = (req, res, next) => {
+  res.json({ message: "Not implemneted YET" });
+};
